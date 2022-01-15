79d78959014fb2345ca603b82aa5f42ce262433d
Osipov Konstantin <k.osipov.msk@gmail.com>
2022-01-15 18:03:36 +0300

    Added command show limit. closes #6

--- commands/showCommand.go
+++ commands/showCommand.go
@@ -29,6 +29,7 @@ func ShowByPrefix(prefix, format string) string {
 	return showByIterator(
 		dbh.NewIterator(util.BytesPrefix([]byte(prefix)), nil),
 		format,
+		0,
 	)
 }
 
@@ -45,13 +46,31 @@ func ShowByRange(start, limit, format string) string {
 	return showByIterator(
 		dbh.NewIterator(&util.Range{Start: []byte(start), Limit: []byte(limit)}, nil),
 		format,
+		0,
+	)
+}
+
+// ShowLimit It shows all content of the database limited by the limit.
+// Use the field `format` for specifying the display format of data.
+// The list of possible values of format options: raw (default), geohash, bson, int64, float64
+//
+// Returns a string containing information about the result of the operation.
+func ShowLimit(limit int, format string) string {
+	if !isConnected {
+		return AppError(ErrDbDoesNotOpen)
+	}
+
+	return showByIterator(
+		dbh.NewIterator(nil, nil),
+		format,
+		limit,
 	)
 }
 
 // Show by iterator
 //
 // Returns a string containing information about the result of the operation.
-func showByIterator(iter iterator.Iterator, format string) string {
+func showByIterator(iter iterator.Iterator, format string, limit int) string {
 	if iter.Error() != nil {
 		return "Empty result!"
 	}
@@ -62,11 +81,18 @@ func showByIterator(iter iterator.Iterator, format string) string {
 
 	w.Init(writer, 0, 8, 0, '\t', 0)
 	fmt.Fprintln(w, "Key\t| Value")
+
+	count := 0
 	for iter.Next() {
 		key := iter.Key()
 		value := iter.Value()
 
 		fmt.Fprintf(w, "%s\t| %s\n", string(key), cliutil.ToString(format, value))
+
+		count++
+		if limit != 0 && count >= limit {
+			break
+		}
 	}
 
 	w.Flush()
